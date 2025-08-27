90cfa20f7daf0e72b59927b4024f13aeacdf4eaa
Abhishek04-dot <guptaabhishek0403@gmail.com>
2025-08-27 15:13:50 +0530

    Add files via upload

---
+++ Module 2_Basic Python Concepts_Task-1.py
@@ -0,0 +1,9 @@
+a= input('Enter first number: ')
+b= input('Enter second number: ')
+a=int(a)
+b=int(b)
+Add = a+b
+sub= a-b
+mul= a*b
+div= a/b
+print('Addition',Add,'\nSubtraction',sub,'\nMultiplication',mul,'\nDivision',div)
